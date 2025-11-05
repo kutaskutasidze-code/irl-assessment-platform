8658f55ae0ee86dbab639cd9a841aeb11d6e9e45
Allone <kutaskutasidze@gmail.com>
2025-11-05 15:48:11 +0400

    Force new build with unique SHA - timestamp update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 // Vercel Serverless API Entry Point
-// Last updated: 2025-11-05
+// Force rebuild: 2025-11-05 11:34 UTC
+// This timestamp ensures a fresh commit SHA
 const app = require('../server-serverless');
 
 module.exports = app;
